3e2a1582ce4c2e96ef7da0f842d21b665949b952
delta71911 <delta71911@naver.com>
2023-05-04 22:57:48 +0900

    2023-05-04

---
+++ src/Ch06/sec11/exam01/KoreanExample.java
@@ -0,0 +1,15 @@
+package Ch06.sec11.exam01;
+
+public class KoreanExample {
+    public static void main(String[] args) {
+        Korean k1 = new Korean("123456-1234567", "김자바");
+
+        System.out.println(k1.nation);
+        System.out.println(k1.ssn);
+        System.out.println(k1.name);
+
+//        k1.nation = "USA";
+//        k1.ssn = "54321-325654";
+        k1.name = "이자바";
+    }
+}
