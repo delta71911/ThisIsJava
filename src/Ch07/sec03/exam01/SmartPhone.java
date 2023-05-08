4b1235283e3794b30b0150321dcc1d92bb33a4bd
delta71911 <delta71911@naver.com>
2023-05-08 11:16:36 +0900

    2023-05-08

---
+++ src/Ch07/sec03/exam01/SmartPhone.java
@@ -0,0 +1,10 @@
+package Ch07.sec03.exam01;
+
+public class SmartPhone extends phone {
+    public SmartPhone(String model, String color) {
+        super();
+        this.model = model;
+        this.color = color;
+        System.out.println("SmartPhone(String model, String color) 생성자 실행됨");
+    }
+}
---
+++ src/Ch07/sec03/exam01/phone.java
@@ -0,0 +1,10 @@
+package Ch07.sec03.exam01;
+
+public class phone {
+    public String model;
+    public String color;
+
+    public phone() {
+        System.out.println("PHone() 생성자 실행");
+    }
+}
