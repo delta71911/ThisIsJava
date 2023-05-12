fb535c9e15bf5017a1309deb06d9445779ad0bb3
delta71911 <delta71911@naver.com>
2023-05-12 09:22:34 +0900

    2023-05-12

---
+++ src/Ch09/sec02/exam01/A.java
@@ -0,0 +1,15 @@
+package Ch09.sec02.exam01;
+
+public class A {
+    class B {}
+
+    B field = new B();
+
+    A() {
+        B b = new B();
+    }
+
+    void method() {
+        B b = new B();
+    }
+}
